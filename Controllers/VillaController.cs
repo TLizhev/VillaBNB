af29c36b2a99f4c3291a477eb71ed35a691fbcce
Todor <84274078+TLizhev@users.noreply.github.com>
2022-01-12 21:54:09 +0000

    Create Add view and Villa view model

---
+++ Controllers/VillaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VillaBNB.Data;
+using VillaBNB.Models;
+
+namespace VillaBNB.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public VillaController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            var view = new VillaViewModel();
+            return View(view);
+        }
+    }
+}
---
+++ Models/VillaViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VillaBNB.Models
+{
+    public class VillaViewModel
+    {
+        public string Name { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int CityId { get; set; }
+
+        public int CountryId { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Bedrooms { get; set; }
+
+        public int Bathrooms { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public decimal PricePerNight { get; set; }
+    }
+}
