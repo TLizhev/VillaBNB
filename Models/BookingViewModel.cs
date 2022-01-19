354bc064ca94a01bcfa6f6f1c2dff88f38ec356d
Todor <84274078+TLizhev@users.noreply.github.com>
2022-01-19 22:48:06 +0000

    Create BookingController and BookingViewModel

---
+++ Models/BookingViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VillaBNB.Models
+{
+    public class BookingViewModel
+    {
+        public int VillaId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public decimal TotalCost { get; set; }
+
+        public int PeopleCount { get; set; }
+    }
+}
