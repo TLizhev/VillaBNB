9d2ed42b78d849f6edee4fa874239a7edd83901e
Todor <84274078+TLizhev@users.noreply.github.com>
2022-01-22 22:15:43 +0000

    Create Villa Services

---
+++ Services/IVillaService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VillaBNB.Services
+{
+    public interface IVillaService
+    {
+    }
+}
---
+++ Services/VillaService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VillaBNB.Data;
+
+namespace VillaBNB.Services
+{
+   public class VillaService :IVillaService
+    {
+        private readonly ApplicationDbContext db;
+
+          
+        public VillaService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+    }
+}
