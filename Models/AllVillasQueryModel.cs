692ec55946e1d8c7cea406b1de6ccb9f6a66c903
Todor <84274078+TLizhev@users.noreply.github.com>
2022-02-03 21:25:47 +0000

    Create AllVillasQueryModel

---
+++ Models/AllVillasQueryModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VillaBNB.Models
+{
+    public class AllVillasQueryModel
+    {
+        public const int VillasPerPage = 3;
+
+        public string Name { get; set; }
+        [Display(Name = "Search by text")]
+        public string SearchTerm { get; set; }
+
+        public int TotalVillas { get; set; }
+
+        public int CurrentPage { get; set; } = 1;
+
+        public VillaSorting Sorting { get; set; }
+
+        public IEnumerable<string> Names { get; set; }
+
+        public IEnumerable<VillaListingModel> Villas { get; set; }
+    }
+}
---
+++ Models/VillaSorting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VillaBNB.Models
+{
+    public enum VillaSorting
+    {
+        DateCreated = 0,
+        City=1,
+        Category =2,
+    }
+}
