f3f1a3fe50e7ae79ddfbfb0ad42acb94ddc5019d
Todor <84274078+TLizhev@users.noreply.github.com>
2022-02-13 19:20:29 +0000

    Create Statistics Services and Model

---
+++ Services/Statistics/StatisticsService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VillaBNB.Data;
+
+namespace VillaBNB.Services.Statistics
+{
+    public class StatisticsService : IStatisticsService
+    {
+
+        private readonly ApplicationDbContext db;
+
+        public StatisticsService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public StatisticsServiceModel Total()
+        {
+            var totalVillas = this.db.Villas.Count();
+            var totalBookings = this.db.Bookings.Count();
+
+            return new StatisticsServiceModel
+            {
+                VillasCount = totalVillas,
+                BookingsCount = totalBookings
+            };
+        }
+    }
+}
