ff6d232ba7963bd3cb632ff4ec547ce1779f21a8
Todor <84274078+TLizhev@users.noreply.github.com>
2022-02-21 22:07:47 +0000

    Small change to User/Owner class

--- Data/Models/Owner.cs
+++ Data/Models/Owner.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using VillaBNB.Data.Models;
@@ -16,7 +17,10 @@ namespace VillaBNB.Models
 
         public string PhoneNumber { get; set; }
 
+        [ForeignKey(nameof(User))]
         public string UserId { get; set; }
+
+        public User User { get; set; }
 
         public IEnumerable<Villa> Villas { get; set; } = new List<Villa>();
 
