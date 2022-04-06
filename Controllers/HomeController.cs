0ab542b60ad3ad513a3c5617522b99d7b17aec93
Todor <84274078+TLizhev@users.noreply.github.com>
2022-04-06 22:18:19 +0100

    Add stuff

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -67,6 +67,11 @@ namespace VillaBNB.Controllers
             return View(query);
         }
 
+        public IActionResult Privacy()
+        {
+            return this.View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
